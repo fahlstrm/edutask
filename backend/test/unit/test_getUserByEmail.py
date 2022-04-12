bc6af588f29290c6c2cdadc7e04877162e318208
Frida <fridaahlstrom@outlook.com>
2022-04-12 13:59:33 +0100

    Added docstring to test and no-match-test case

--- backend/test/unit/test_getUserByEmail.py
+++ backend/test/unit/test_getUserByEmail.py
@@ -6,38 +6,48 @@ from src.util.dao import DAO
 
 
 def test_noAt_ValueError():
+    """ Test that ValueError raised when email dont validate """
     mockeddao = mock.MagicMock() # mock the dependency
-    mockeddao.find.return_value = [{'email': 'frah20@student.bth.se'}, {'email': 'frah20@student.bth.se'}] # define the behavior
     mockedsut = UserController(dao=mockeddao) # inject the dependency and instantiate an object of class UserController
 
     with pytest.raises(ValueError):
         test_email = "frah20student.bth.se" # no @
-        validationresult = mockedsut.get_user_by_email(test_email)
+        mockedsut.get_user_by_email(test_email)
+
+def test_noMatchFound_true():
+    mockeddao = mock.MagicMock() # mock the dependency
+    mockeddao.find.return_value = [] # define the behavior
+    mockedsut = UserController(dao=mockeddao) # inject the dependency and instantiate an object of class UserController
+
+    test_email = "joki20@student.bth.se"
+    validationresult = mockedsut.get_user_by_email(test_email)
+    assert validationresult == None
+
 
 def test_singleMatchFound_true():
+    """ Test that email is returned when match """
     mockeddao = mock.MagicMock() # mock the dependency
     mockeddao.find.return_value = [{'email': 'joki20@student.bth.se'}] # define the behavior
     mockedsut = UserController(dao=mockeddao) # inject the dependency and instantiate an object of class UserController
-
 
     test_email = "joki20@student.bth.se"
     validationresult = mockedsut.get_user_by_email(test_email)
     assert validationresult == { "email": test_email }
 
 def test_severalMatchFound_true():
-    
+    """ Test that only 1st is returned when several email matches """
     mockeddao = mock.MagicMock() # mock the dependency
-    mockeddao.find.return_value = [{'email': 'joki20@student.bth.se'}, {'email': 'joki20@student.bth.se'}] # define the behavior
+    mockeddao.find.return_value = [{'email': 'joki20@student.bth.se', 'name': 'Johan'}, {'email': 'joki20@student.bth.se', 'name': 'Frida'}] # define the behavior
     mockedsut = UserController(dao=mockeddao) # inject the dependency and instantiate an object of class UserController
 
     test_email = "joki20@student.bth.se"
     validationresult = mockedsut.get_user_by_email(test_email)
-    assert validationresult == { "email": test_email }
+    assert validationresult == {'email': 'joki20@student.bth.se', 'name': 'Johan'}
 
 def test_Exception_true():
-    mockeddao = mock.MagicMock() # mock the dependency
-    mockedsut = UserController({"shit"}) # inject the dependency and instantiate an object of class UserController
+    """ Test that exception is rasised """
+    mockedsut = UserController({}) # inject the dependency and instantiate an object of class UserController
 
     with pytest.raises(Exception):
         test_email = "frah20@student.bth.se" # no @
-        validationresult = mockedsut.get_user_by_email(test_email)
+        mockedsut.get_user_by_email(test_email)
