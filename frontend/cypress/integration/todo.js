b5a7334c5a379905cd1de59e27b954bb8138c1ec
Johan Kristoffersson <joki20@student.bth.se>
2022-04-26 20:04:30 +0100

    AssertionError for use case 1 with red border

--- frontend/cypress/integration/todo.js
+++ frontend/cypress/integration/todo.js
@@ -16,22 +16,42 @@ describe('Todo', () => {
 
             //Login
             cy.contains('div', 'Email Address').find('input').type(user.email)
-            cy.get('form').submit()
+            cy.get('.submit-form').submit()
 
             //Control first page
             cy.get('h1').should('contain.text', `Your tasks, ${user.firstName} ${user.lastName}`)
 
-            //Select first task
+            // //Select first task
             cy.get('a img:first').click()
         })
     })
 
 
     it('Add Task, input complete', () => {
-        //Get the form by class inline-form, type in the input field 
-        cy.get('.inline-form').type("This is a test")
+        // Get the form by class inline-form, type in the input field
+        var todoText = "This is a test";
+
+        cy.get('.inline-form').type(todoText)
 
         //Submit the form
         cy.get('.inline-form').submit()
+
+        //Check last li matches text
+        cy.get('.todo-item:last').should('contain.text', todoText)
+    })
+
+    it('Add Task, input empty', () => {
+        //Get the form by class inline-form, type empty string
+        cy.get('.inline-form input[type=text]').should('have.value', '');
+
+        // get button, invoke disabled attribute and try to click it (force: true prevents error message in cypress)
+        cy.get('.inline-form input[type=submit')
+            .invoke('attr', 'disabled')
+            .then(disabled =>{
+                disabled ? cy.get('.inline-form input[type=submit').click({force: true}) : null
+            })
+        
+        // returns AssertionError: 'expected ... red, but the value was 'rgb(204, 204, 204)' 
+        cy.get('.inline-form input[type=text').should('have.css', 'border-color', 'red')
     })
 })
